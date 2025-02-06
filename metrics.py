c1297378ac2b5e785ea4de568c64ceaf81324915
arsburz <burzilow.ars@yandex.ru>
2025-02-06 21:54:34 +0300

    The Grid class has been implemented, which divides a given part of the plane into cells, and for each cell stores a list of points lying in it. With the help of this class, a similarity metric has been added, comparing the density of points in the corresponding cells. A similarity metric has also been implemented, using the closest lidar point for each radar point.

---
+++ metrics.py
@@ -0,0 +1,61 @@
+import numpy as np
+from scipy.spatial import KDTree
+from read_and_prepare_files import RadarPoint, LidarPoint, Point, Data
+
+class Grid():
+    def __init__(self, data: list[Point], row_num: int = 100, col_num: int = 100, l_border: float = -130,
+                 r_border: float = 130, top_border: float = -130, bottom_border: float = 130):
+        self.row_num = row_num
+        self.col_num = col_num
+        
+        self.l_border = l_border
+        self.r_border = r_border
+        self.top_border = top_border
+        self.bottom_border = bottom_border
+        
+        self.data = defaultdict(list)
+        self.size = len(data)
+        for point in data:
+            
+            i = int(col_num * (point.x - l_border) / (r_border - l_border))
+            j = int(row_num * (point.y - bottom_border) / (top_border - bottom_border))
+
+            self.data[i,j].append(point)
+            
+    def count(i: int, j: int) -> int:
+        return d[i, j].count()
+    
+    def size() -> int:
+        return self.size
+        
+    def density(i: int, j: int) -> float:
+        return self.count(i, j) / self.size
+        
+def DensityMetric(lid_cloud: list[Point], rad_cloud: list[Point]) -> float:
+    grid_1 = Grid(lid_cloud)
+    grid_2 = Grid(rad_cloud)
+    s = 0
+    keys = grid_1.data.keys() | grid_2.data.keys()
+    for key in keys:
+        i, j = key
+        s += (grid_2.density(i, j) - grid_2.density(i, j)) ** 2
+    return s ** 0.5
+
+def from_point_to_pair(points: list[Point]):
+    return [(p.x, p.y) for p in points]
+
+def find_nearest_lidar_points(lidar_points: list[Point], radar_points: list[Point]):
+    #Находит ближайшую лидарную точку для каждой радарной точки на плоскости и расстояние до нее.
+    lid_points = from_point_to_pair(lidar_points)
+    rad_points = from_point_to_pair(radar_points)
+    
+    lidar_tree = KDTree(lid_points)  
+    distances, indices = lidar_tree.query(rad_points) 
+    return distances, [lid_points[i] for i in indices]
+    
+def NearestPointMetric(lid_cloud: list[Point], rad_cloud: list[Point]) -> float:
+    lid_points = from_point_to_pair(lid_cloud)
+    rad_points = from_point_to_pair(rad_cloud)
+    distances = find_nearest_lidar_points(lid_points, rad_points)[0]
+    return ((distances ** 2).sum()) ** 0.5
+                                     
